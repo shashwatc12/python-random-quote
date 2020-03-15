02bba5018efdb53a319e175ff29b27ee72297b73
Shashwat Chavan <spchavan@outlook.com>
2020-03-15 02:20:43 -0400

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,8 @@
 def main():
-   print("Keep it logically awesome.")
-
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+     f = open("quotes.txt")
+     quotes = f.readlines()
+     f.close()
+     print(quotes)
 
 if __name__== "__main__":
   main()
